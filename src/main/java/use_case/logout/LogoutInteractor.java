4c0e64ada2aebe1c7cd89bfffa73091c881afc94
Lenovo <franklin.mao@mail.utoronto.ca>
2025-11-27 06:42:43 -0500

    A great change

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -9,15 +9,20 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
     }
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
+        // Get the current username before we clear it
+        String username = userDataAccessObject.getCurrentUsername();
         // * set the current username to null in the DAO
+        userDataAccessObject.setCurrentUsername(null);
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username);
         // * tell the presenter to prepare a success view.
+        logoutPresenter.prepareSuccessView(logoutOutputData);
     }
 }
 
--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -108,8 +108,14 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      * @param evt the ActionEvent to react to
      */
     public void actionPerformed(ActionEvent evt) {
-        // TODO: execute the logout use case through the Controller
         System.out.println("Click " + evt.getActionCommand());
+        if (evt.getSource().equals(logOut)) {
+            if (logoutController != null) {
+                logoutController.execute();
+            } else {
+                System.out.println("logoutController not good!");
+            }
+        }
     }
 
     @Override
@@ -140,6 +146,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
